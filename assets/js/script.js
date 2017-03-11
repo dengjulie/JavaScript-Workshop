83843d067ff2b14b39c9dc1bbd28688547b2f8a9
Jeff Gong <jgong@berkeley.edu>
2017-03-10 17:30:12 -0800

    Remove solutions and add code stubs

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,11 +19,9 @@ $(document).ready(function() {
   /* ACTIVITY 1
    * TIPS: Use the jQuery CSS function in order to insert CSS into the HTML element */
   $('#box1').hover(function() {
-    $('#box1').css("height", 200);
-    $('#box1').css("width", 200);
+    // YOUR CODE HERE
   }, function() {
-    $('#box1').css("height", 100);
-    $('#box1').css("width", 100);
+    // YOUR CODE HERE
   })
 
 
@@ -32,7 +30,7 @@ $(document).ready(function() {
    * How do we add a class to an HTML element? Some functions you might want to use: toggleClass
    * Important: the id's characteristics always override any of the classes' characteristics (see this by trying to modify left) */
   $('#box2').click(function() {
-    $('#box2').toggleClass('box-shifted');
+    // YOUR CODE HERE
   })
 
   /* ACTIVITY 3:
@@ -41,12 +39,7 @@ $(document).ready(function() {
    * manner as above?
    * TIPS: Some functions you might want to use: hasClass, removeClass, addClass, show, hide. */
   $('#box3').click(function() {
-    if ($('#box3').hasClass('box-color')) {
-      $('#box3').removeClass('box-color');
-    }
-    else {
-      $('#box3').addClass('box-color');
-    }
+    // YOUR CODE HERE
   })
 
   /* ACTIVITY 4:
@@ -55,17 +48,11 @@ $(document).ready(function() {
    * When you "hide" a box, we can't click on it anymore, so what can we do with this box to help us?
    * TIPS: You might want to use the functions: show, hide, css */
   $('#box4').click(function() {
-    if ($('#box4').is(":visible")) {
-      $('#box4').hide();
-      $('#showbox4').css({'visibility' : 'visible'});
-    }
+    // YOUR CODE HERE
   })
 
   $('#showbox4').click(function() {
-    if ($('#box4').is(":hidden")) {
-      $('#box4').show();
-      $('#showbox4').css({'visibility' : 'hidden'});
-    }
+    // YOUR CODE HERE
   })
 
   /* ACTIVITY 5:
@@ -74,10 +61,7 @@ $(document).ready(function() {
    *  BONUS: Use setTimeout to make events happen at different times
    *  using animate will override id characteristics (try modifying left!) */
   $('#box5').click(function() {
-    $('#box5').animate({height: '300px', left: '50%'}, 200);
-    setTimeout(function() {
-      $('#box5').animate({height: '100px', left: '75%'}, 200);
-    })
+    // YOUR CODE HERE
   })
 
 
@@ -86,15 +70,11 @@ $(document).ready(function() {
    * Hint: Use the following: click, hasClass, removeClass, addClass, and an if-else clause */
   $("#sidebar-button").click(function() {
     if ($(".sidebar-container").hasClass("sidebar-active")) {
-      $("body").removeClass("no-scroll");
-      $("#sidebar-button").removeClass("button-active");
-      $(".sidebar-container").removeClass("sidebar-active");
-      $(".page-wrapper").removeClass("wrapper-active");
+      // YOUR CODE HERE
     } else {
-      $("#sidebar-button").addClass("button-active");
-      $(".sidebar-container").addClass("sidebar-active");
-      $(".page-wrapper").addClass("wrapper-active");
-      // the following is a bit more challenging but gets rid of a bug with scrolling
+      // YOUR CODE HERE
+
+      // the following handles a bug with scrolling, don't worry about it
       setTimeout(function() {
       $("body").addClass("no-scroll");
       }, 300);
@@ -103,10 +83,7 @@ $(document).ready(function() {
 
   $(".page-wrapper").click(function() {
     if ($(".sidebar-container").hasClass("sidebar-active")) {
-      $("body").removeClass("no-scroll");
-      $("#sidebar-button").removeClass("button-active");
-      $(".sidebar-container").removeClass("sidebar-active");
-      $(".page-wrapper").removeClass("wrapper-active");
+      // YOUR CODE HERE
     }
   });
 
@@ -115,17 +92,14 @@ $(document).ready(function() {
    * Or, for smooth scrolling, can use the animate function which takes in two parameters - one for the function, and one for the time it animates over */
 
   $("#home-link").click(function() {
-    $("html, body").animate({ scrollTop: 0 }, 300);
-    return false;
+    // YOUR CODE HERE
   })
 
   $("#misc-link").click(function() {
-    $("html, body").animate({ scrollTop: 500 }, 300);
-    return false;
+    // YOUR CODE HERE
   })
   $("#dog-link").click(function() {
-    $("html, body").animate({ scrollTop: 1200 }, 300);
-    return false;
+    // YOUR CODE HERE
   })
 
   /* Extra Challenge:
@@ -138,22 +112,10 @@ $(document).ready(function() {
    * Google jQuery functions! */
 
   $("#carousel-next").click(function() {
-    var margin = parseInt($('#carousel').css('margin-left').replace("px", ""));
-    if (margin <= -3840) {
-      return False;
-    }
-    else {
-      $("#carousel").css("margin-left", margin - 960 + "px");
-    }
+    // YOUR CODE HERE
   });
 
   $("#carousel-prev").click(function() {
-    var margin = parseInt($('#carousel').css('margin-left').replace("px", ""));
-    if (margin >= 0) {
-      return False;
-    }
-    else {
-      $("#carousel").css("margin-left", margin + 960 + "px");
-    }
+    // YOUR CODE HERE
   });
 });
